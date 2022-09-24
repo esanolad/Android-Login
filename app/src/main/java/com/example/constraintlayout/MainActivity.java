4a717690429dbfa86c47057aa6fe5d10cc992904
esanolad <esanolad@gmail.com>
2022-09-24 10:00:28 +0400

    adds otp check

--- app/src/main/java/com/example/constraintlayout/MainActivity.java
+++ app/src/main/java/com/example/constraintlayout/MainActivity.java
@@ -10,15 +10,17 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    EditText email, password;
+    EditText txt_email, txt_password, txt_otp;
     Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        email=findViewById(R.id.editTextTextEmailAddress);
-        password= findViewById(R.id.editTextTextPassword);
+        txt_email=findViewById(R.id.editTextTextEmailAddress);
+        txt_password= findViewById(R.id.editTextTextPassword);
         login = findViewById(R.id.btn_main_login);
+        txt_otp = findViewById(R.id.txt_main_otp);
+
     }
 
     @Override
@@ -27,13 +29,23 @@ public class MainActivity extends AppCompatActivity {
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String emailVal = email.getText().toString();
-                String pwd = password.getText().toString();
+                String emailVal = txt_email.getText().toString();
+                String pwd = txt_password.getText().toString();
                 String sr = "hemanth@gmail.com";
                 String ps = "changeMe";
                 if (emailVal.equals(sr)  && pwd.equals(ps)) {
-                    Intent i = new Intent(MainActivity.this, Welcome.class);
-                    startActivity(i);
+                    String otp = txt_otp.getText().toString();
+                    if (otp.equals("1234")) {
+                        Intent i = new Intent(MainActivity.this, Welcome.class);
+                        Bundle b = new Bundle();
+                        b.putString("email", sr);
+                        i.putExtras(b);
+                        startActivity(i);
+                    }
+                    else {
+                        Toast.makeText(MainActivity.this, "Incorrect OTP", Toast.LENGTH_SHORT).show();
+                    }
+
                 }
                 else {
                     Toast.makeText(MainActivity.this, "Incorrect login", Toast.LENGTH_SHORT).show();
--- app/src/main/java/com/example/constraintlayout/Welcome.java
+++ app/src/main/java/com/example/constraintlayout/Welcome.java
@@ -3,12 +3,24 @@ package com.example.constraintlayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Welcome extends AppCompatActivity {
-
+    TextView email;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        email = findViewById(R.id.lbl_welcome_email);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Bundle b = getIntent().getExtras();
+        email.setText(b.getString("email"));
+        //Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
     }
 }
